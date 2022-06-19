af4e42bcb7aaafa30a4a18580742a73035ec15d9
Roman <kybik_rybik1994@mail.ru>
2022-06-19 19:07:11 +0300

    add typescript

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Button: FC = () => {
+  return <button></button>;
+};
+
+export default Button;
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,15 +1,32 @@
-import React, { FC } from "react";
+import { FC } from "react";
+import * as React from "react";
 
 const Header: FC = () => {
   return (
     <div className="main-page__header">
-      <div>Мой профиль</div>
-      <ul>
-        <li>Поиск</li>
-        <li>Уведомления</li>
-        <li>Для глазастых</li>
-        <li>Аватар</li>
-      </ul>
+      <div className="header-bottom__title">Мой профиль</div>
+      <div className="header-bottom__menu">
+        <div className="header-bottom__search">
+          <img
+            src="my-app/src/etc/img/search.svg"
+            alt=""
+            width="50"
+            height="50"
+          />
+        </div>
+        <div className="header-bottom__bell">
+          <img src="" alt="" />
+        </div>
+        <div className="header-bottom__eye">
+          <img src="" alt="" />
+        </div>
+        <div className="header-bottom__avatar">
+          <img src="" alt="" />
+        </div>
+        <div className="header-bottom__group">
+          <img src="" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
