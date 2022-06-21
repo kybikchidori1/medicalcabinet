1fd2bada80a1f3ca2a2d23d7acf8a3a97666d965
Roman <kybik_rybik1994@mail.ru>
2022-06-21 21:33:20 +0300

    add components icon side-bar

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -1,15 +1,35 @@
 import React, { FC } from "react";
+import Book from "../../etc/img/icons/Book";
+import Heart from "../../etc/img/icons/Heart";
+import Speak from "../../etc/img/icons/Speak";
+import Test from "../../etc/img/icons/Test";
+import Vector from "../../etc/img/icons/Vector";
 
 const Sidebar: FC = () => {
   return (
     <div className="side-bar">
       <div className="side-bar__item">
         <div className="side-bar__logo">Логотип</div>
-        <div className="side-bar__link">Профиль</div>
-        <div className="side-bar__link">Врачи и клиники</div>
-        <div className="side-bar__link">Сообщения</div>
-        <div className="side-bar__link">Тестирование</div>
-        <div className="side-bar__link">Полезно знать</div>
+        <div className="side-bar__link">
+          <Heart />
+          Профиль
+        </div>
+        <div className="side-bar__link">
+          <Vector />
+          Врачи и клиники
+        </div>
+        <div className="side-bar__link">
+          <Speak />
+          Сообщения
+        </div>
+        <div className="side-bar__link">
+          <Test />
+          Тестирование
+        </div>
+        <div className="side-bar__link">
+          <Book />
+          Полезно знать
+        </div>
         <button className="side-bar__btn">Подать заявку</button>
         <div>
           <a className="side-bar__help" href="/">
