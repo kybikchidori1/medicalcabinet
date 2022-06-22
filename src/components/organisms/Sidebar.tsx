17e99ff81a28aaf6687bdea0f29710c09dd35f25
Roman <kybik_rybik1994@mail.ru>
2022-06-22 20:51:05 +0300

    add side-bar style and icon

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import Book from "../../etc/img/icons/Book";
 import Heart from "../../etc/img/icons/Heart";
+import Help from "../../etc/img/icons/Help";
 import Speak from "../../etc/img/icons/Speak";
 import Test from "../../etc/img/icons/Test";
 import Vector from "../../etc/img/icons/Vector";
@@ -11,28 +12,29 @@ const Sidebar: FC = () => {
       <div className="side-bar__item">
         <div className="side-bar__logo">Логотип</div>
         <div className="side-bar__link">
-          <Heart />
+          <Heart className="side-bar__icon" />
           Профиль
         </div>
         <div className="side-bar__link">
-          <Vector />
+          <Vector className="side-bar__icon" />
           Врачи и клиники
         </div>
         <div className="side-bar__link">
-          <Speak />
+          <Speak className="side-bar__icon" />
           Сообщения
         </div>
         <div className="side-bar__link">
-          <Test />
+          <Test className="side-bar__icon" />
           Тестирование
         </div>
         <div className="side-bar__link">
-          <Book />
+          <Book className="side-bar__icon" />
           Полезно знать
         </div>
         <button className="side-bar__btn">Подать заявку</button>
-        <div>
-          <a className="side-bar__help" href="/">
+        <div className="side-bar__">
+          <a href="/" className="side-bar__help">
+            <Help />
             Помощь
           </a>
         </div>
